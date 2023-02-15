c757d5023d77bf4efef5220f2db8157f315e1c48
IvanMasnari <i.masnari@exin.digital>
2023-02-15 21:45:18 +0100

    prima versione upload_feed()

--- feed.py
+++ feed.py
@@ -1,7 +1,10 @@
 from base64 import b64decode
+import random
+import time
 from PIL import Image
 from openai_image import openai_api, outpaint, merge
 from news import news
+from instagram_bot import bot
 from pathlib import Path
 
 FEED_DIR = Path.cwd() / 'feed'
@@ -17,6 +20,15 @@ CENTER_DIR.mkdir(exist_ok=True)
 LEFT_DIR.mkdir(exist_ok=True)
 
 DIR_LIST = [RIGHT_DIR, CENTER_DIR, LEFT_DIR]
+
+PROMPT = [
+    "Tesla might have revealed the Cybertruck frame in a new video about the automaker’s robots.\n more…\nThe post Did Tesla leak the Cybertruck frame with this robot? appeared first on Electrek.",
+    "As AI hype hits a fever pitch, CNBC's Brandon Gomez and Fahiemah Al-Ali kick off a conversation around the widespread interest in the technology with Paper c...",
+    "Doge, Floki and SHIB are up, while BONK is flat after Musk declares his dog Floki is an amazing CEO of Twitter and “better than that other guy.”",
+    "Egg prices have surged 70% in January on an annual basis. Egg prices have been on the rise over the past year due to inflation and continued outbreaks of bird flu.",
+    "Stocks making the biggest moves midday: Roblox, Airbnb, Barclays, Silvergate Capital & more - CNBC",
+    "The U.S. economy is expected to grow at a 4.5% annual rate in the fourth quarter, according to the Atlanta Fed's GDPNow forecast released on Wednesday.",
+]
 
 PROMPT_DECORATOR = "Comment on the following piece of news as if you were a Roman pagan historian of the imperial era:\n"
 
@@ -93,8 +105,49 @@ def feed_seed():
             image.save(f"{index}_{article['title']}.png")
         except Exception as e:
             continue
-    
+
+def generate_next_feed():
+    current_index = 0
+    current_image = None
+    for dir in DIR_LIST:
+        for file in dir.iterdir():
+            #if file is png 
+            if file.suffix == ".png":
+                #split file name on _ and get first element
+                index = int(file.name.split("_")[0])
+                if index > current_index:
+                    current_index = index
+                    current_image = file
+    if current_image:
+        print(f"Making tile for {current_image.name}")
+        try:
+            prompt = random.choice(PROMPT)
+            image = outpaint.make_tile(image, prompt, "top")
+            image.save(f"{index}_{prompt}.png")
+        except Exception as e:
+            pass
+        
+            
+def upload_feed():
+    dir_list_reverse = DIR_LIST[::-1]
+    for dir in dir_list_reverse:
+        for file in dir.iterdir():
+            #if file is png 
+            if file.suffix == ".png":
+                #split file name on _ and get first element
+                index = int(file.name.split("_")[0])
+                if index > current_index:
+                    current_index = index
+                    current_image = file
+    if current_image:
+        print("uploading feed")
+        try:
+            bot.upload_photo(path=current_image, caption=current_image.name.split("_")[1])
+            print("uploaded feed")
+            time.sleep(60)
+        except Exception as e:
+            pass
 
 
 if __name__ == "__main__":
-    feed_seed()
+    upload_feed()
